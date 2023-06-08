e0dd3829c54a46b463f0df7c1e44cba3b839a8d7
huyennk6 <huyennk6@fpt.edu.vn>
2023-06-08 11:22:09 +0700

    LinQ continue...

--- NET102_SD18301/BAI_17_LINQ/Program.cs
+++ NET102_SD18301/BAI_17_LINQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,7 +37,18 @@ namespace BAI_17_LINQ
         #endregion
         //Data source
         static string[] arrName = { "Hoang", "Hoa", "Luc", "Long", "Tuan", "Nam", "Tung" };
-        static List<int> arrNumber = new List<int>() { 1,2,3,4,5,6,7,8,9,10};
+        static List<int> lstNumber = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //sửa tên => tổ hợp phím Ctrl R (2 lần) hoặc click chuột chọn Rename
+        static List<Movie> lstMovies = new List<Movie>()
+        {
+            new Movie(Guid.NewGuid(), "Big Mouse", "Hanh dong", 2022,new List<string>(){"Huyen","Duc","Hung"}),
+            new Movie(Guid.NewGuid(), "The Little Mermaid", "Lang man", 2020,new List<string>(){"Huyen","Duc","Hung"}),
+            new Movie(Guid.NewGuid(), "Doreamon", "Hoat hinh", 2018,new List<string>(){"Huyen","Duc","Hung"}),
+            new Movie(Guid.NewGuid(), "Fast & Furious", "Hanh dong", 2015,new List<string>(){"Huyen","Duc","Hung"}),
+            new Movie(Guid.NewGuid(), "OnePice", "Kinh di", 2022,new List<string>(){"Huyen","Duc","Hung"}),
+            new Movie(Guid.NewGuid(), "OnePice", "Tam ly", 2022,new List<string>(){"Huyen","Duc","Hung"}),
+        };
+
         #region VD1: LINQ QUERY
         static void ViDu1()
         {
@@ -50,9 +62,9 @@ namespace BAI_17_LINQ
             }
             Console.WriteLine("--------------------");
             //Linq query
-            var list= from obj in arrName
-                      where obj.StartsWith("T")
-                      select obj;
+            var list = from obj in arrName
+                       where obj.StartsWith("T")
+                       select obj;
             //query execute
             foreach (var item in list)
             {
@@ -72,28 +84,49 @@ namespace BAI_17_LINQ
           */
         static void ViDu2()
         {
-            int min = arrNumber.Min();
-            Console.WriteLine("min= "+ min);
+            int min = lstNumber.Min();
+            Console.WriteLine("min= " + min);
 
-            var sum = (from num in arrNumber
+            var sum = (from num in lstNumber
                        where num > 5
                        select num).Sum();
-            Console.WriteLine("Sum= "+ sum);
-            var agg = arrNumber.Aggregate((a, b) => a * b);//1*2*3*4*5*6.....
-            Console.WriteLine("agg= "+ agg);
+            Console.WriteLine("Sum= " + sum);
+            var agg = lstNumber.Aggregate((a, b) => a * b);//1*2*3*4*5*6.....
+            Console.WriteLine("agg= " + agg);
             var concat = arrName.Aggregate((a, b) => a + "-" + b);
-            Console.WriteLine("concat= "+ concat);
+            Console.WriteLine("concat= " + concat);
+
+            //Đếm tất cả phim có thể loại là phim Hành động
+            //cách 1:
+            //String.Compare(str1, str2, <bool ignoreCase>)
+            //true -> ko phân biệt , false-> có phân biệt 
+            var countHD = lstMovies.Where(
+                             x => String.Compare(x.Type, "Hanh dong", true) == 0).Count();
+            Console.WriteLine("1. Số lượng phim hành động : "+ countHD);
+            //cách 2: 
+            var countTheLoai = lstMovies.Count(m => m.Type.Equals("Hanh dong"));
+            Console.WriteLine("2. Số lượng phim hành động : " + countTheLoai);
+
+            //Tìm ra năm sản xuất mới nhất
+            //In ra các bộ phim mới nhất
+            //List<Movie> lstSortNewest = lstMovies.OrderByDescending(x => x.Year).ToList();
+            //int newYear = lstSortNewest[0].Year;
+            //var newYearFilm = lstMovies.Where(x => x.Year == newYear);
+            //foreach (var item in newYearFilm)
+            //{
+            //    item.InThongTin();
+            //}
+            Console.WriteLine("---Phim mới nhất------------");
+            var yearNewest = lstMovies.Max(m => m.Year);
+            var movieNewest = lstMovies.Where(m => m.Year == yearNewest);
+            foreach (var item in movieNewest)
+            {
+                item.InThongTin();
+            }
+
         }
         #endregion
 
-        static List<Movie> lstMovies = new List<Movie>()
-        {
-            new Movie(Guid.NewGuid(), "Big Mouse", "Hanh dong", 2022,new List<string>(){"Huyen","Duc","Hung"}),
-            new Movie(Guid.NewGuid(), "The Little Mermaid", "Lang man", 2020,new List<string>(){"Huyen","Duc","Hung"}),
-            new Movie(Guid.NewGuid(), "Doreamon", "Hoat hinh", 2018,new List<string>(){"Huyen","Duc","Hung"}),
-            new Movie(Guid.NewGuid(), "Fast & Furious", "Hanh dong", 2015,new List<string>(){"Huyen","Duc","Hung"}),
-            new Movie(Guid.NewGuid(), "OnePice", "Kinh di", 2022,new List<string>(){"Huyen","Duc","Hung"}),
-        };
         #region  VD3: Sorting Operator in LINQ
         /*
          * Sorting Operators in LINQ dùng thay đổi thứ tự tăng dần hoặc giảm dần 
@@ -101,22 +134,70 @@ namespace BAI_17_LINQ
          */
         static void ViDu3()
         {
+            //sử dụng OrderBy
+            Console.WriteLine("---------Sắp xếp theo tên----------");
             var sortName = lstMovies.OrderBy(s => s.Name);
+            //lstMovies[0].Id = Guid.Parse(Console.ReadLine());
             foreach (var item in sortName)
             {
                 item.InThongTin();
+
             }
             //Linq Query
-            //......
+            Console.WriteLine("---------Sắp xếp theo thể loại----------");
+            var sortType = from m in lstMovies
+                           orderby m.Type descending
+                           select m;
+            //Type: thuộc tính của phim
+            //ascending: tăng dần - descending: giảm dần
+            foreach (var item in sortType)
+            {
+                item.InThongTin();
+            }
+            Console.WriteLine("---------Sắp xếp theo năm -> sau đó là thể loại----------");
+            var sortYear_Type = lstMovies.OrderBy(a => a.Year).ThenByDescending(a => a.Type);
+            foreach (var item in sortYear_Type)
+            {
+                item.InThongTin();
+            }
         }
-        #endregion  
+        #endregion
+
+        #region VD4: Partition Operator
+        /*
+         * Take Partition Operator: 
+         * Toán tử này sẽ lấy N phần tử đầu tiên trong một tập dữ liệu
+         * 
+         * TakeWhile Partition Operator:
+         * Phương thức mở rộng TakeWhile trong LINQ 
+         * trả về các phần tử từ phần tử đầu tiên cho đến khi một phần tử không thỏa mãn điều kiện. 
+         * Nếu chính phần tử đầu tiên không thỏa mãn điều kiện thì trả về một danh sách trống.
+         */
+        static void ViDu4()
+        {
+            Console.WriteLine("-----Take---------");
+            var takeMovie = lstMovies.Take(3);
+            foreach (var item in takeMovie)
+            {
+                item.InThongTin();
+            }
+            Console.WriteLine("------Take while------");
+            var takeWhileYear = lstMovies.TakeWhile(x => x.Year == 2022);
+            foreach (var item in takeWhileYear)
+            {
+                item.InThongTin();
+            }
+            //skip - skip while
+        }
+        #endregion
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
             //ViDu1();
             //ViDu2();
-            ViDu3();
+            //ViDu3();
+            ViDu4();
         }
     }
 }
